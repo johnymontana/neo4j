dca849c644458858f768be91714c0d068127ca4c
johnymontana <lyonwj@gmail.com>
2014-06-03 10:56:18 -0600

    Properly mock Node in DistanceTest Fix #1
    Use Mockito.when to mock node.getProperty()
    Add additional unit tests for Node, WKT POINT combination

--- community/cypher/cypher-compiler-2.1/src/test/scala/org/neo4j/cypher/internal/compiler/v2_1/commands/DistanceTest.scala
+++ community/cypher/cypher-compiler-2.1/src/test/scala/org/neo4j/cypher/internal/compiler/v2_1/commands/DistanceTest.scala
@@ -11,37 +11,67 @@ import org.neo4j.graphdb.Node
 import org.scalatest.mock.MockitoSugar
 import org.neo4j.cypher.internal.compiler.v2_1.commands.expressions.DistanceFunction
 import org.neo4j.cypher.internal.compiler.v2_1.commands.expressions.Literal
+import org.mockito.Mockito
 
-/** Test Distance function
+
+/** DistanceFunction tests
  *
  */
 class DistanceTest extends Assertions with MockitoSugar {
+  /**
+   * Test WKT POINT(x y), POINT (x y)
+   */
   @Test def givenAStringAndStringReturnDistance() {
     val func = new DistanceFunction(Literal("POINT (15.34 20)"), Literal("POINT (20 10)"))
     calc(func).asInstanceOf[Double] should be (11.03 +- 0.01)
   }
 
+  /**
+   * Test Node{x,y}, Node{x,y}
+   */
   @Test def givenANodeAndNodeReturnDistance() {
-    val node1 = mock[Node]
-    val node2 = mock[Node]
+    val node1 = mockedNodeWithXY(1, 15.34, 20.0)
+    val node2 = mockedNodeWithXY(2, 20.0, 10.0)
     val queryContext = mock[QueryContext]
-
-    node1.setProperty("x", 12.2)
-    node1.setProperty("y", 10.2)
-
-    node2.setProperty("x", 99.1)
-    node2.setProperty("y", 33.4)
-
 
     val state = QueryStateHelper.emptyWith(query = queryContext)
     val ctx = ExecutionContext() += ("n1" -> node1) += ("n2" -> node2)
-
-    // FIXME: how to mock nodes with properties? This doesn't seem to work
     val result = DistanceFunction(Identifier("n1"), Identifier("n2"))(ctx)(state)
 
     result.asInstanceOf[Double] should be (11.03 +- 0.01)
-    // FIXME: this test fails, result evaluates to 0.0
+
+  }
+
+  /**
+   * Test Node{x,y}, WKT POINT(x y)
+   */
+  @Test def givenNodeStringReturnDistance() {
+    val node = mockedNodeWithXY(1, 15.34, 20.0)
+    val wkt_point = "POINT (20.0 10.0)"
+    val queryContext = mock[QueryContext]
+    val state = QueryStateHelper.emptyWith(query = queryContext)
+    val ctx  = ExecutionContext() += ("n" -> node)
+
+    val result = DistanceFunction(Identifier("n"), Literal(wkt_point))(ctx)(state)
+
+    result.asInstanceOf[Double] should be (11.03 +- 0.01)
   }
 
   private def calc(e: Expression): Any = e(ExecutionContext.empty)(QueryStateHelper.empty)
+
+
+  /** Mock Node with x, y attributes
+   *
+   * @param id
+   * @param x
+   * @param y
+   * @return
+   */
+  private def mockedNodeWithXY(id: Long, x: Double, y: Double): Node = {
+    val node = mock[Node]
+
+    Mockito.when(node.getProperty("x").asInstanceOf[Double]).thenReturn(x)
+    Mockito.when(node.getProperty("y").asInstanceOf[Double]).thenReturn(y)
+    node
+  }
 }
