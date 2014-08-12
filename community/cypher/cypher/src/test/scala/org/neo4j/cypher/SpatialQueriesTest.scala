3c614d9d5b9d55599c33da0b3ac464554f67a9c4
Craig Taverner <craig@amanzi.com>
2014-08-12 17:30:27 +0200

    Fixed scala syntax bug

--- community/cypher/cypher/src/test/scala/org/neo4j/cypher/SpatialQueriesTest.scala
+++ community/cypher/cypher/src/test/scala/org/neo4j/cypher/SpatialQueriesTest.scala
@@ -9,7 +9,6 @@ class SpatialQueriesTest extends ExecutionEngineFunSuite {
     val result = engine.execute("CREATE (n1:WKTLayer {name: 'node1', geom: 'POINT(32.1 22.5)'})" +
       "WITH spatialCreateLayer('WKTLayer', 'WKT') AS layer, n1 " +
       "WITH spatialAddNode(n1, layer) AS node1, layer, n1 " +
-      ""
       "RETURN n1.name as n");
 
     result.toList should equal(List(Map("n"->"node1")))
